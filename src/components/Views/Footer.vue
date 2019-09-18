062120335802b9d94debdc085f31762df7f6ea80
Andres <gomez_andresf@hotmail.com>
2019-09-18 10:54:19 -0500

    Ajuste de footer

--- src/components/Views/Footer.vue
+++ src/components/Views/Footer.vue
@@ -1,5 +1,5 @@
 <template>
     <p class="footer-copyright mb-0 py-3 text-center">
-    Términos y condiciones de uso | Politicas de privacidad
+    Términos y condiciones de uso | Politicas de privacidad &nbsp;
     </p>
 </template>
